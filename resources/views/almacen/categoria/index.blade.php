1a52f10ee687a91d7dba92bb65f7dc5b91211c1c
Dario Pilacuan <dario.pilacuan@epn.edu.ec>
2019-08-16 19:21:47 -0500

    mi primer comit
    inicio

---
+++ resources/views/almacen/categoria/index.blade.php
@@ -0,0 +1,47 @@
+@extends ('layouts.admin')
+@section ('contenido')
+<div class="row">
+
+<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+	<h3>Listado de Categorias <a href="categoria/create"><button class="bn btn-success">Nuevo</button></a></h3>
+	@include ('almacen.categoria.search')
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+	
+	<div class="table-responsive">
+		<table class="table table-striped table-bordered table-condesed table-hover">
+		<thead>
+			<th>Id</th>
+			<th>Nombre</th>
+			<th>Descripcion</th>
+			<th>Opciones</th>
+			</thead>
+			@foreach ($categorias as $cat)
+			
+			<tr>
+				<td>{{ $cat->idcategoria}}</td>
+				<td>{{ $cat->nombre}}</td>
+				<td>{{ $cat->descripcion}}</td>
+				<td>
+				<a href="{{URL::action('CategoriaController@edit', $cat->idcategoria)}}"><button class="btn btn-info">Editar</button></a>
+				<a href="" data-target="#modal-delete-{{$cat->idcategoria}}" data-toggle="modal"><button class="btn btn-danger">Eliminar</button></a>
+					
+				</td>
+			
+			</tr>
+			@include('almacen.categoria.modal')
+			@endforeach
+			
+			
+		
+		</table>
+		
+		</div>
+		{{$categorias->render()}}
+	</div>
+
+</div>
+@endsection
